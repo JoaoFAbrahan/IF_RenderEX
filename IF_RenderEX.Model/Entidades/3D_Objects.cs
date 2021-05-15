e6d60f393c11d45824417f301845a3f2f731cc55
Joao Felipe Abrahan <joaofelipeabrahan@hotmail.com>
2021-05-15 13:18:02 -0400

    Add project files.

---
+++ IF_RenderEX.Model/Entidades/3D_Objects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// DirectX 11 Dependências
+using SharpDX;
+using SharpDX.DXGI;
+
+namespace IF_RenderEX.Model
+{
+    public class Class_3D_Objects
+    {
+    }
+}
